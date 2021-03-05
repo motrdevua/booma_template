4b51520b6cbc4ac2358af5727c9179682ed3642c
Igor Lukashev <motrdevua@gmail.com>
2021-03-05 14:29:41 +0200

    fix

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -5,14 +5,14 @@ const paddingOffset = `${window.innerWidth - document.body.offsetWidth}px`;
 
 const buttonOpenModal = document.querySelectorAll('.modal__open');
 const modalContent = document.querySelectorAll('[data-windowid]');
-let contentHTML = '';
+let modalContentHTML = '';
 
 class Modal {
   constructor() {
     const wrapper = document.querySelector('.wrapper');
     const modal = this;
 
-    modal.open = function () {
+    modal.create = function (className) {
       modal.wrapper = document.createElement('div');
       modal.window = document.createElement('div');
       modal.content = document.createElement('div');
@@ -20,6 +20,7 @@ class Modal {
 
       modal.wrapper.classList.add('modal__wrapper');
       modal.window.classList.add('modal');
+      modal.window.classList.add(className);
       modal.content.classList.add('modal__content');
       modal.closeButton.classList.add('modal__close');
 
@@ -27,16 +28,19 @@ class Modal {
       modal.wrapper.appendChild(modal.window);
       modal.window.appendChild(modal.closeButton);
       modal.window.appendChild(modal.content);
+    };
 
+    modal.open = function () {
       modal.wrapper.classList.add('modal__wrapper-active');
       modal.window.classList.add('modal-fade-in');
       modal.closeButton.classList.add('modal__close-active');
 
-      modal.content.innerHTML = contentHTML;
+      modal.content.innerHTML = modalContentHTML;
 
       document.body.style.overflow = 'hidden';
       document.body.style.paddingRight = paddingOffset;
       document.querySelector('.header').style.paddingRight = paddingOffset;
+      document.querySelector('.footer').style.paddingRight = -paddingOffset;
 
       return modal;
     };
@@ -49,6 +53,7 @@ class Modal {
       document.body.style.overflow = 'unset';
       document.body.style.paddingRight = '0';
       document.querySelector('.header').style.paddingRight = '0';
+      document.querySelector('.footer').style.paddingRight = '0';
 
       return modal;
     };
@@ -66,7 +71,8 @@ const modal = new Modal();
 function addingContent(btn) {
   for (let i = 0; i < modalContent.length; i += 1) {
     if (modalContent[i].dataset.windowid === btn.dataset.buttonid) {
-      contentHTML = modalContent[i].innerHTML;
+      modal.create(modalContent[i].dataset.type);
+      modalContentHTML = modalContent[i].innerHTML;
     }
   }
 }
